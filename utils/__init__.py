5ada3c53dc7d1e1e3e351cd706b7e33e7182f629
gwenwen007 <gwendolin.potter242@gmail.com>
2026-04-02 23:59:30 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,4 @@
-#Placeholder ← Makes utils/ importable as a package
+# This file makes utils/ a Python package so you can do:
+#   from utils.api import fetch_electricity_prices
+#   from utils.model import predict_prices
+#   etc.
